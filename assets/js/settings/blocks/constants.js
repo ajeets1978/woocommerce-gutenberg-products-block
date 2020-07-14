1d24e6868ba73c2a4b74f9a550ddc78c89e77774
Rua Haszard <rua.haszard@automattic.com>
2020-07-14 12:14:50 +1200

    revert signup form - checkout block will always generate username etc

--- assets/js/settings/blocks/constants.js
+++ assets/js/settings/blocks/constants.js
@@ -100,11 +100,3 @@ export const CHECKOUT_ALLOWS_SIGNUP = getSetting(
 	'checkoutAllowsSignup',
 	false
 );
-export const SIGNUP_GENERATE_USERNAME = getSetting(
-	'signupGenerateUsername',
-	false
-);
-export const SIGNUP_GENERATE_PASSWORD = getSetting(
-	'signupGeneratePassword',
-	false
-);
